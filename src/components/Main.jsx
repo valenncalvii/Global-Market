8e11509eeff1e98e6570c4d63bf550d63c5d9bb2
valentin calvi <vcalvi@frsn.utn.edu.ar>
2024-10-08 19:13:47 -0300

    productcard agregado y archivos cambiados

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,31 @@
 import React from "react";
-
+import ProductCards from "./ProductCards";
 export default function Main() {
-  return(
+  const products = [
+    {
+      image: "https://picsum.photos/300?random=1",
+      title: "Producto 1",
+      price: 19.99,
+    },
+    { image: "https://picsum.photos/300?random=2", title: "Producto 2", price: 19.99 },
+    { image: "https://picsum.photos/300?random=3", title: "Producto 3", price: 19.99 },
+  ];
+
+  return (
     <main className="main-container">
-    {/* Aquí va el contenido principal de la página, como los productos */}
-    <section>
-      <h2>Productos Destacados</h2>
-      <div className="product-grid">
-        {/* Aquí puedes agregar la lógica para mostrar productos */}
-      </div>
-    </section>
-  </main>
+      {/* Aquí va el contenido principal de la página, como los productos */}
+      <section className="product-list">
+        <h2>Productos Destacados</h2>
+
+        {products.map((product, index) => (
+          <ProductCards
+            key={index}
+            image={product.image}
+            title={product.title}
+            price={product.price}
+          />
+        ))}
+      </section>
+    </main>
   );
 }
