3fe27647ced1491ad4c28194fcef575e1599f15b
valentin calvi <vcalvi@frsn.utn.edu.ar>
2024-11-12 21:52:51 -0300

    commit especial para martin

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -11,17 +11,16 @@ const Login = () => {
   const location = useLocation(); // Hook para obtener la URL desde la que el usuario vino
   const from = location.state?.from || "/"; // Si no existe la URL de referencia, redirige a productos
 
-  const handleSubmit = async(values)=>{
+  const handleSubmit = async (values) => {
     try {
-      
-       // Realizar el POST para login
-       const response = await axios.post(`${API_URL}/auth/login`, {
+      // Realizar el POST para login
+      const response = await axios.post(`${API_URL}/auth/login`, {
         username: values.username,
         email: values.email,
-        password: values.password
+        password: values.password,
       });
       setMessage("Inicio de sesión exitoso");
-      localStorage.setItem('authToken', response.data.token);
+      localStorage.setItem("authToken", response.data.token);
       navigate(from, { replace: true });
     } catch (error) {
       console.error("Detalles del error:", error);
@@ -29,62 +28,95 @@ const Login = () => {
       // Manejar diferentes tipos de errores
       if (error.response) {
         // El servidor respondió con un código fuera del rango 2xx
-        setMessage("Error en el inicio de sesión: " + (error.response.data?.message || "Detalles no disponibles."));
+        setMessage(
+          "Error en el inicio de sesión: " +
+            (error.response.data?.message || "Detalles no disponibles.")
+        );
       } else if (error.request) {
         // La solicitud fue hecha pero no hubo respuesta
-        setMessage("Error en el inicio de sesión: No se recibió respuesta del servidor.");
+        setMessage(
+          "Error en el inicio de sesión: No se recibió respuesta del servidor."
+        );
       } else {
         // Error en la configuración de la solicitud
         setMessage("Error en el inicio de sesión: " + error.message);
       }
     }
-  }
-
-    
+  };
 
   return (
     <div className="login-container">
       <h2>Iniciar Sesión</h2>
-     
-        <Formik
-          initialValues={{username:"", email: "", password: "" }}
-          validate={(values) => {
-            const errors = {};
-            if (!values.username) {
-              errors.email = "Se necesita un nombre";
-            }
-            if (!values.email) {
-              errors.email = "Se necesita un correo";
-            }
-            if (!values.password) {
-              errors.password = 'Se necesita una contraseña';
-            }
-            return errors;
-          }}
-          onSubmit={handleSubmit}
-        >
-           {({ values, errors }) => (
+
+      <Formik
+        initialValues={{ username: "", email: "", password: "" }}
+        validate={(values) => {
+          const errors = {};
+          if (!values.username) {
+            errors.username = "Se necesita un nombre";
+          }
+          if (!values.email) {
+            errors.email = "Se necesita un correo";
+          }
+          if (!values.password) {
+            errors.password = "Se necesita una contraseña";
+          }
+          return errors;
+        }}
+        onSubmit={handleSubmit}
+      >
+        {({ values, errors }) => (
           <Form>
-             <div className="form-group">
-              <label>Nombre:</label>
-              <Field type="text" id="username" name="username" value={values.username} />
-              <ErrorMessage name="username" component={() =>(<div className="error-message">{errors.username}</div>)}/>
+            <div className="form-group">
+              <label htmlFor="username">Nombre:</label>
+              <Field
+                type="text"
+                id="username"
+                name="username"
+                value={values.username}
+              />
+              <ErrorMessage
+                name="username"
+                component={() => (
+                  <div className="error-message">{errors.username}</div>
+                )}
+              />
             </div>
             <div className="form-group">
-              <label>Email:</label>
-              <Field type="email" id="email" name="email" value={values.email} />
-              <ErrorMessage name="email" component={() =>(<div className="error-message">{errors.email}</div>)}/>
+              <label htmlFor="email">Email:</label>
+              <Field
+                type="email"
+                id="email"
+                name="email"
+                value={values.email}
+              />
+              <ErrorMessage
+                name="email"
+                component={() => (
+                  <div className="error-message">{errors.email}</div>
+                )}
+              />
             </div>
             <div className="form-group">
-              <label>Contraseña:</label>
-              <Field type="password" id="password" name="password" value={values.password} />
-              <ErrorMessage name="password" component={() =>(<div className="error-message">{errors.password}</div>)}/>
+              <label htmlFor="password">Contraseña:</label>
+              <Field
+                type="password"
+                id="password"
+                name="password"
+                value={values.password}
+              />
+              <ErrorMessage
+                name="password"
+                component={() => (
+                  <div className="error-message">{errors.password}</div>
+                )}
+              />
             </div>
             <button type="submit">Iniciar Sesión</button>
-            {message && <p>{message}</p> }
+            {message && <p>{message}</p>}
           </Form>
         )}
-        </Formik>
+      </Formik>
 
       <p className="register-link">
         ¿No tienes una cuenta? <a href="/register">Regístrate aquí</a>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -5,58 +5,69 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import axios from "axios";
 import API_URL from "../API";
 const Register = () => {
-
-  const [message, setMessage] = useState('');
-
+  const [message, setMessage] = useState("");
 
   return (
     <div className="register-container">
       <h2>Registrarse</h2>
       <Formik
-        initialValues={{username: '', email: '', password: '', confirmPassword: '' }}
-        validate={values => {
+        initialValues={{
+          username: "",
+          email: "",
+          password: "",
+          confirmPassword: "",
+        }}
+        validate={(values) => {
           const errors = {};
           //error nombre
-          if(!values.username){
-            errors.username = 'se necesita un nombre'
+          if (!values.username) {
+            errors.username = "se necesita un nombre";
           }
           //error correo
           if (!values.email) {
-            errors.email = 'Se necesita un correo';
+            errors.email = "Se necesita un correo";
           } else if (
             !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
           ) {
-            errors.email = '"El correo debe tener un formato válido: solo letras, números, puntos, guiones y un dominio de al menos dos letras después del punto (ej., .com)."';
+            errors.email =
+              '"El correo debe tener un formato válido: solo letras, números, puntos, guiones y un dominio de al menos dos letras después del punto (ej., .com)."';
           }
           //error password
           if (!values.password) {
-            errors.password = 'Se necesita una contraseña';
+            errors.password = "Se necesita una contraseña";
           } else if (
             !/^[A-Za-z0-9!@#$%^&*()_+.,-]{8,16}$/i.test(values.password)
           ) {
-            errors.password = '"la contraseña debe tener entre 8 y 16 caracteres y puede incluir letras, números y símbolos';
+            errors.password =
+              '"la contraseña debe tener entre 8 y 16 caracteres y puede incluir letras, números y símbolos';
           }
-          if(values.confirmPassword !== values.password){
-            errors.confirmPassword = 'las contraseñas no coinciden'
+          if (values.confirmPassword !== values.password) {
+            errors.confirmPassword = "las contraseñas no coinciden";
           }
 
           return errors;
         }}
-        onSubmit={ async (values, {resetForm}) => {
-              try{
-                const {confirmPassword, ...filteredValues} = values
-                const response = await axios.post(`${API_URL}/users`, filteredValues);
-                console.log(message)
-                setMessage("registro exitoso!" + (response.data.message || ""));
-              }catch(error){
-                setMessage("error en el registro: " +(error.response?.data?.message || error.message));
-              }
+        onSubmit={async (values, { resetForm }) => {
+          try {
+            const { confirmPassword, ...filteredValues } = values;
+            const response = await axios.post(
+              `${API_URL}/users`,
+              filteredValues
+            );
+            console.log(message);
+            setMessage("registro exitoso!" + (response.data.message || ""));
+          } catch (error) {
+            setMessage(
+              "error en el registro: " +
+                (error.response?.data?.message || error.message)
+            );
+          }
           resetForm();
         }}
       >
         {({ values, errors }) => (
           <Form className="register-form">
-             <div className="form-group">
+            <div className="form-group">
               <label htmlFor="username">Nombre:</label>
               <Field
                 type="text"
@@ -64,7 +75,12 @@ const Register = () => {
                 name="username"
                 value={values.username}
               />
-              <ErrorMessage name="username" component={() =>(<div className="error-message">{errors.username}</div>)}/>
+              <ErrorMessage
+                name="username"
+                component={() => (
+                  <div className="error-message">{errors.username}</div>
+                )}
+              />
             </div>
             <div className="form-group">
               <label htmlFor="email">Email:</label>
@@ -74,7 +90,12 @@ const Register = () => {
                 name="email"
                 value={values.email}
               />
-              <ErrorMessage name="email" component={() =>(<div className="error-message">{errors.email}</div>)}/>
+              <ErrorMessage
+                name="email"
+                component={() => (
+                  <div className="error-message">{errors.email}</div>
+                )}
+              />
             </div>
             <div className="form-group">
               <label htmlFor="password">Contraseña:</label>
@@ -84,7 +105,12 @@ const Register = () => {
                 name="password"
                 value={values.password}
               />
-              <ErrorMessage name="password" component={() =>(<div className="error-message">{errors.password}</div>)}/>
+              <ErrorMessage
+                name="password"
+                component={() => (
+                  <div className="error-message">{errors.password}</div>
+                )}
+              />
             </div>
             <div className="form-group">
               <label htmlFor="confirmPassword">Confirmar Contraseña:</label>
@@ -93,7 +119,12 @@ const Register = () => {
                 id="confirmPassword"
                 name="confirmPassword"
               />
-              <ErrorMessage name="confirmPassword" component={() =>(<div className="error-message">{errors.confirmPassword}</div>)}/>
+              <ErrorMessage
+                name="confirmPassword"
+                component={() => (
+                  <div className="error-message">{errors.confirmPassword}</div>
+                )}
+              />
             </div>
 
             <button type="submit" name="submit" className="register-button">
@@ -112,10 +143,7 @@ const Register = () => {
 
 export default Register;
 
-
-
-
 /*
   orders: comparar userid con orden de userid, seria compra de productos. que muestre cada compra en carrito que se hizo, guardar en order los productos en carrito
 
-  */ 
+  */
