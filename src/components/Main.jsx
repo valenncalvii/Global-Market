b0b4af83f35b4589d8311b861415ba2dff83b638
MauroRomero1 <mromero@frsn.utn.edu.ar>
2024-11-14 03:35:36 -0300

    Filtro de categorias Funcionando

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,28 +1,24 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import ProductCards from "./ProductCards";
 import axios from "axios";
 import Pagination from "./pagination";
 import API_URL from "../services/API";
 import "../styles/ProductCard.css";
-import { Cloudinary } from '@cloudinary/url-gen';
-import { auto } from '@cloudinary/url-gen/actions/resize';
-import { autoGravity } from '@cloudinary/url-gen/qualifiers/gravity';
-import { AdvancedImage } from '@cloudinary/react';
 
 const ITEMS_PER_PAGE = 9;
 
 export default function Main() {
   const [currentPage, setCurrentPage] = useState(1);
-  const [filterCategory, setFilterCategory] = useState("");
+  const [filterCategory, setFilterCategory] = useState(null);
   const [productos, setProductos] = useState([]);
   const [categories, setCategories] = useState([]);
 
+  // Función para obtener productos
   useEffect(() => {
     const obtenerProductos = async () => {
       try {
         const response = await axios.get(`${API_URL}/products`);
         setProductos(response.data);
-        console.log("Categorías disponibles:", response.data.map(product => product.category));
       } catch (error) {
         console.error("Error al obtener productos:", error);
       }
@@ -30,13 +26,12 @@ export default function Main() {
     obtenerProductos();
   }, []);
 
-  // Obtener categorías desde el endpoint
+  // Función para obtener categorías
   useEffect(() => {
     const fetchCategories = async () => {
       try {
-        const response = await axios.get(`${API_URL}/products/{id}`);
+        const response = await axios.get(`${API_URL}/category`);
         setCategories(response.data);
-       console.log(categories)
       } catch (error) {
         console.error("Error al obtener categorías:", error);
       }
@@ -44,22 +39,23 @@ export default function Main() {
     fetchCategories();
   }, []);
 
-  //ACA ESTA EL FILTRO POR CATEGORIA PERO ESTA MEDIO MAL, ALGO DE ACA SIRVE IGUAL
-  
-   // Filtrar productos por categoría
-   const filterProducts = () => {
-    if (!filterCategory) return productos;  // Si no hay categoría seleccionada, mostrar todos los productos
-    return productos.filter(product => product.category?.nombre === filterCategory);
-    console.log("aca"+filterCategory)
-    console.log("otro"+productos) 
-
+  // Función para manejar la selección de categorías
+  const handleCategorySelection = (category) => {
+    setFilterCategory(category); // Establece el filtro de categoría
+    setCurrentPage(1); // Reinicia la página a la primera cuando se cambia la categoría
   };
-  //HASTA ACA EL FILTRO DSP ABAJO ES PARA FILTRAR LAS PAG QUE APAREZCAN 9 PROD
-  
-  const filteredProducts = filterProducts();
 
+    // Filtrar productos por categoría
+    const filterProducts = () => {
+      if (!filterCategory) return productos; // Si no hay categoría seleccionada, mostrar todos los productos
+      return productos.filter(
+        (product) => product.categoryId === filterCategory.id // Asegúrate de que los tipos de category_id e id sean compatibles
+      );
+    };
 
-  // Para la paginación que muestre la cantidad de páginas
+    const filteredProducts = filterProducts();
+
+  // Configuración de paginación
   const totalFilteredPages = Math.ceil(filteredProducts.length / ITEMS_PER_PAGE);
   const indexOfLastProduct = currentPage * ITEMS_PER_PAGE;
   const indexOfFirstProduct = indexOfLastProduct - ITEMS_PER_PAGE;
@@ -69,26 +65,18 @@ export default function Main() {
     <main className="main-container">
       <aside className="filter-container">
         <div className="filter-product-box">
-          <h3>Construcción</h3>
+          <h3>Categorías</h3>
           <ul>
-            <li><button onClick={() => setFilterCategory("Sanitarios")}>Sanitarios</button></li>
-            <li><button onClick={() => setFilterCategory("Materiales")}>Materiales</button></li>
-            <li><button onClick={() => setFilterCategory("Herramientas varias")}>Herramientas varias</button></li>
-          </ul>
-
-          <h3>Ferretería</h3>
-          <ul>
-            <li><button onClick={() => setFilterCategory("herramientas de corte")}>Herramientas de corte</button></li>
-            <li><button onClick={() => setFilterCategory("herramientas de fijacion")}>Herramientas de fijación</button></li>
-            <li><button onClick={() => setFilterCategory("herramientas de mano")}>Herramientas de mano</button></li>
-            <li><button onClick={() => setFilterCategory("herramientas electricas")}>Herramientas eléctricas</button></li>
-            <li><button onClick={() => setFilterCategory("herrajes y herraduras")}>Herrajes y herraduras</button></li>
-          </ul>
-          
-          <h3>Hogar</h3>
-          <ul>
-            <li><button onClick={() => setFilterCategory("jardin y camping")}>Jardín y Camping</button></li>
-            <li><button onClick={() => setFilterCategory("piletas")}>Piletas</button></li>
+            {categories.map((category) => (
+              <li key={category.id}>
+                <button onClick={() => handleCategorySelection(category)}>
+                  {category.nombre}
+                </button>
+              </li>
+            ))}
+            <li>
+              <button onClick={() => handleCategorySelection(null)}>Mostrar todos</button>
+            </li>
           </ul>
         </div>
       </aside>
